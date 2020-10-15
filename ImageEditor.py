daffcc452baae8b379bd4ed99b969563ad22510d
maximilienmoraud <maximillien.moraud@etu.imt-lille-douai.fr>
2020-10-15 17:50:21 +0200

    on peut faire des petits points

--- ImageEditor.py
+++ ImageEditor.py
@@ -3,8 +3,12 @@ from tkinter import *
 from tkinter import filedialog
 import numpy as np
 from tkinter.messagebox import *
+
 from PIL import ImageTk, Image
 
+x = 0
+y = 0
+list = []
 
 
 class Editor:
@@ -19,7 +23,7 @@ class Editor:
         self.SelectionType.insert(3, "  Other")
         self.SelectionType.pack()
 
-        self.EditButton = tk.Button(self.master, text='Edit', width=25, command=self.SelectionStart)
+        self.EditButton = tk.Button(self.master, text='Edit', width=25, command=test)
         self.EditButton.pack(side=LEFT)
 
         self.ExitButton = tk.Button(self.master, text='Quit', width=25, command=self.CloseWindow)
@@ -32,15 +36,16 @@ class Editor:
     def Image(self):
         # Fonction qui permet d'afficher l'image
         img = ImageTk.PhotoImage(ResizeImage(Image.open("Images/PhotoChat.jpg"), self.master.winfo_screenwidth()))
-        self.canvas = tk.Canvas(self.master, width=img.width(), height=img.height(), borderwidth=0, highlightthickness=0)
+        self.canvas = tk.Canvas(self.master, width=img.width(), height=img.height(), borderwidth=0,
+                                highlightthickness=0)
         self.canvas.pack(expand=True)
         self.canvas.img = img
         self.canvas.create_image(0, 0, image=img, anchor=tk.NW)
         self.canvas.bind('<Button-1>', GetMousePos)
+        self.canvas.bind('<Motion>', self.Mouvement)
 
-    def SelectionStart(self):
-        if self.SelectionType.get(ACTIVE) == " Square":
-            print(x, y)
+    def Mouvement(self, zz):
+        self.canvas.create_oval(x - 1, y - 1, x + 1, y + 1, width=2, outline="black")
 
 
 def ResizeImage(img, ScreenWidth):
@@ -51,7 +56,12 @@ def ResizeImage(img, ScreenWidth):
 def GetMousePos(event):
     global x
     global y
+    global list
     x = event.x
     y = event.y
+    pos = x, y
+    list.append(pos)
 
 
+def test():
+    print(list)
